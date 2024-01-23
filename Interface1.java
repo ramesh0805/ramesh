9b43ab8eefe50490ec7c2027a24929e44ae2ff63
ramesh0805 <140526626+ramesh0805@users.noreply.github.com>
2024-01-23 19:17:48 +0530

    Update Interface1.java

--- Interface1.java
+++ Interface1.java
@@ -1,69 +1,5 @@
 package vamsi;
 import java.io.*;
-/*public class Interface1 {
-    public static void main(String[] args) {
-        Person4 p = new Employee1();
-        p.read();
-        p.display(); 
-        p.EAA();
-        Person4.CAA();
-    }
-
-
-
-interface Person4{
-	 String college = "GPREC"; //public static final
-	 abstract void read();
-	 void display();
-	 
-	 default void EAA() {
-     	System.out.println("Sports");
-     	System.out.println("Yoga");
-     	System.out.println("NSS");
-     	System.out.println("VCS");
-        Internship();
-     }
-	 
-	 static void CAA() {
-     	System.out.println("Bhuvanavijayam");
-     }
-     
-     private void Internship() {
-     	System.out.println("Summer Internship");
-
-     }
-
-}
-
-class Employee1 implements Person4{
-    String name;
-    double salary;
-	@Override
-	public void read() {
-		// TODO Auto-generated method stub
-		name = "xyz";
-		salary = 10930;
-	}
-
-	@Override
-	public void display() {
-		name = "xyz";
-		salary = 10930;
-		System.out.println("Employee name: " +name);
-		System.out.println("Employee Salary: " +salary);
-		System.out.println("Employee name: " +name);
-	}
-}*/
-
-
-
-
-
-
-
-
-
-
 public class Interface1 {
     public static void main(String[] args) {
         Person4 p = new Employee1();
@@ -116,7 +52,3 @@ class Employee1 implements Person4 {
         System.out.println("Employee Salary: " + salary);
     }
 }
-
-
-
-
